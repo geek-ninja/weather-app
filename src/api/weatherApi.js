993d106ed6495cee10452bd8129929f4e8f283e1
Sanket Puhan <54413046+geek-ninja@users.noreply.github.com>
2022-03-29 12:36:20 +0530

    Update weatherApi.js

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?'
-const apiToken = '012edb9fcc6f6f72da4d1be12644f61d'
+const apiToken = 'your token'
 
 export const getWeather = (city) => axios.get(`${url}q=${city}&units=metric&appid=${apiToken}`)
